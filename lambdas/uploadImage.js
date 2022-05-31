6476009d89c62d6d070e4fed94ef338b6a6c956c
Krystian <krystianjonca17@gmail.com>
2022-05-31 18:30:27 +0200

    Added image upload

--- lambdas/uploadImage.js
+++ lambdas/uploadImage.js
@@ -1,19 +1,75 @@
-'use strict';
+const AWS = require('aws-sdk');
+const { fromBuffer } = require('file-type');
+const { v4 } = require('uuid');
+
+const S3 = new AWS.S3();
+const allowedMimes = ['image/jpeg', 'image/jpg', 'image/png'];
 
 const handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v3.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+  try {
+    const body = JSON.parse(event.body);
+
+    if (!body || !body.image || !body.mime) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          error:
+            'Please provide valid image (jpeg, jpg, png) and mime in the request body.',
+        }),
+      };
+    }
+    if (!allowedMimes.includes(body.mime)) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          error: 'Please provide valid image format (jpeg, jpg, png)',
+        }),
+      };
+    }
+    let imageData = body.image;
+
+    if (imageData.substr(0, 7) === 'base64,') {
+      imageData = imageData.substr(7, imageData.length);
+    }
+
+    const buffer = Buffer.from(imageData, 'base64');
+    const { ext, mime } = await fromBuffer(buffer);
+
+    if (mime !== body.mime) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          error: 'Mimes do not match',
+        }),
+      };
+    }
+
+    const id = v4();
+    const key = `${id}.${ext}`;
+
+    await S3.putObject({
+      Body: buffer,
+      Key: key,
+      ContentType: mime,
+      Bucket: 'serverless-newsletter-list-photos-s3-bucket',
+      ACL: 'public-read',
+    }).promise();
+
+    const url = `https://serverless-newsletter-list-photos-s3-bucket.s3-eu-central-1.amazonaws.com/${key}`;
+    console.log(key);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ imageURL: url }),
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Error while uploading the image.' }),
+    };
+  }
 };
 
-module.export = {
+module.exports = {
   handler,
 };
