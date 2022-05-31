06e3e5eee871d2ad795086cb95589e6085405d52
Krystian <krystianjonca17@gmail.com>
2022-05-31 14:38:50 +0200

    Fixed an error and added fetchAll func

--- lambdas/emailAddress/add.js
+++ lambdas/emailAddress/add.js
@@ -4,10 +4,8 @@ const documentClient = new AWS.DynamoDB.DocumentClient();
 const validateEmail = (email) => {
   let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-  if (email.match(regexEmail)) 
-    return true; 
-  else 
-    return false; 
+  if (email.match(regexEmail)) return true;
+  else return false;
 };
 
 const handler = async (event) => {
@@ -22,21 +20,23 @@ const handler = async (event) => {
     };
   }
 
-  const createdAt = new Date();
+  const createdAt = new Date().toISOString();
   const newEmail = {
     email,
     createdAt,
   };
 
   try {
-    await documentClient.put({
-      TableName: process.env.TABLE_NAME,
-      Item: newEmail,
-    });
+    await documentClient
+      .put({
+        TableName: 'EmailsTable',
+        Item: newEmail,
+      })
+      .promise();
   } catch (error) {
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: 'Error while adding news Email.' }),
+      body: JSON.stringify({ error: 'Error while adding new Email.' }),
     };
   }
   console.log(`New Email created: ${newEmail.email}`);
--- lambdas/emailAddress/fetchAll.js
+++ lambdas/emailAddress/fetchAll.js
@@ -1,19 +1,24 @@
-'use strict';
+const AWS = require('aws-sdk');
+const documentClient = new AWS.DynamoDB.DocumentClient();
 
 const handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v3.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+  try {
+    const { Items: emails } = await documentClient
+      .scan({ TableName: 'EmailsTable' })
+      .promise();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ data: emails }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Error while fetching emails.' }),
+    };
+  }
 };
 
-module.export = {
+module.exports = {
   handler,
 };
