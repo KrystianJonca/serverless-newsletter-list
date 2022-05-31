a9fbed6da9e7b9f1115031073947bc436cc61f1e
Krystian <krystianjonca17@gmail.com>
2022-05-31 13:40:12 +0200

    Created add email function

--- lambdas/emailAddress/add.js
+++ lambdas/emailAddress/add.js
@@ -1,19 +1,45 @@
-'use strict';
+const AWS = require('aws-sdk');
+const { validate } = require('email-validator');
+
+const documentClient = new AWS.DynamoDB.DocumentClient();
 
 const handler = async (event) => {
+  const { email } = JSON.parse(event.body);
+
+  if (!email || !validate(email)) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        error: 'Please provide valid email in the request body.',
+      }),
+    };
+  }
+
+  const createdAt = new Date();
+  const newEmail = {
+    email,
+    createdAt,
+  };
+
+  try {
+    await documentClient.put({
+      TableName: process.env.TABLE_NAME,
+      Item: newEmail,
+    });
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Error while adding news Email.' }),
+    };
+  }
+  console.log(`New Email created: ${newEmail.email}`);
+
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v3.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    body: JSON.stringify(newEmail),
   };
 };
 
-module.export = {
+module.exports = {
   handler,
 };
